d8c7f18b760da7bb573f38e179ce935a23ed9b2d
Aghost <youri_van_sorgen2@example.com>
2021-08-19 13:48:48 +0200

    init commit

---
+++ EnglishNumber.App/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+using static EnglishNumber.Lib.NumbersToEnglish;
+
+namespace EnglishNumber.App
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            /*
+                Tests();
+            */
+
+            if (args.Length > 0 && int.TryParse(args[0], out int number)) {
+                WriteLine(Convert(number));
+            } else {
+                WriteLine("not a number.");
+            }
+        }
+
+        static void Tests() {
+            int[] values = new int[] { 1, 19, 99, 999, 1999, 19345, 123456, 12345678, 1234567890 };
+
+            foreach (int i in values) {
+                WriteLine($"{i} = {Convert(i)}");
+            }
+        }
+    }
+}
---
+++ EnglishNumber.Lib/NumbersToEnglish.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnglishNumber.Lib
+{
+    public static class NumbersToEnglish
+    {
+        public static string[] EnglishDigits = new string[] {
+            "", "One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine ", "Ten ",
+            "Eleven ", "Twelve ", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+        };
+
+        public static string[] EnglishTens = new string[] {
+            "", "", "Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ", "Ninety "
+        };
+
+        public static Dictionary<int, string> NumbersToPlace = new Dictionary<int, string> {
+            {100, "Hundred "}, {1000, "Thousand, "}, {1000000, "Million, "}, {1000000000, "Billion, "}
+        };
+
+
+        public static string ConvertToDigit(int n, string _suffix) {
+            if (n == 0) {
+                return "";
+            }
+
+            return n > 19 ? EnglishTens[n / 10] + EnglishDigits[n % 10] + _suffix : EnglishDigits[n] + _suffix;
+        }
+
+        public static string Convert(int n) {
+            string res;
+
+            res = ConvertToDigit(n % 100, "");
+
+            if (n > 100 && n % 100 >= 1) {
+                res = "and " + res;
+            }
+
+            foreach (var kvp in NumbersToPlace) {
+                res = ConvertToDigit((n / kvp.Key % 100), kvp.Value) + res;
+            }
+
+            return res;
+        }
+    }
+}
